a1e5a1fe60906a5ad77fd2b04bd05b9a8fc2937f
Cmhk.Juwan <juwanchen@hk.chinamobile.com>
2021-11-11 09:27:47 +0800

    change repo

--- deploy.sh
+++ deploy.sh
@@ -23,6 +23,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果你要部署在 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:MleMoe/webar-vue-demo.git main:gh-pages
+git push -f git@github.com:cmhkjuwan/webar-vue-demo.git main:gh-pages
 
 cd -
